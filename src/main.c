f44c9eaa807ffe4cc48ee4062749841284232538
tcharuel <tcharuel@student.42.fr>
2024-01-04 18:46:40 +0100

    moving forward

--- src/main.c
+++ src/main.c
@@ -6,30 +6,46 @@
 /*   By: tcharuel <tcharuel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 13:51:54 by tcharuel          #+#    #+#             */
-/*   Updated: 2024/01/04 17:32:22 by tcharuel         ###   ########.fr       */
+/*   Updated: 2024/01/04 18:24:16 by tcharuel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-int	main(void)
+typedef struct s_vars
 {
 	void	*mlx;
 	void	*win;
+}			t_vars;
 
-	// Initialiser la connexion avec le système graphique
-	mlx = mlx_init();
-	// Vérifier si l'initialisation a réussi
-	if (!mlx)
-		return (1);
+int	close_window(int keycode, t_vars *vars)
+{
+	(void)keycode;
+	mlx_destroy_window(vars->mlx, vars->win);
+	return (0);
+}
 
-	// Créer une nouvelle fenêtre graphique
-	win = mlx_new_window(mlx, 800, 600, "Ma fenetre MiniLibX");
-	// Vérifier si la création de la fenêtre a réussi
-	if (!win)
-		return (1);
+int	main(int argc, char **argv)
+{
+	t_vars	vars;
+	t_data	img;
 
-	// Lancer la boucle d'événements pour traiter les entrées utilisateur
-	mlx_loop(mlx);
+	if (argc == 2)
+	{
+		(void)(argv);
+		vars.mlx = mlx_init();
+		if (!vars.mlx)
+			return (1);
+		vars.win = mlx_new_window(vars.mlx, WINDOW_WIDTH, WINDOW_HEIGHT, "FdF");
+		if (!vars.win)
+			return (1);
+		img.img = mlx_new_image(vars.mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
+		img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel,
+				&img.line_length, &img.endian);
+		my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+		mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
+		mlx_hook(vars.win, 2, 1L << 0, close_window, &vars);
+		mlx_loop(vars.mlx);
+	}
 	return (0);
 }
--- src/utils.c
+++ src/utils.c
@@ -1,35 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fdf.h                                              :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tcharuel <tcharuel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/06 13:59:01 by tcharuel          #+#    #+#             */
-/*   Updated: 2024/01/04 18:24:06 by tcharuel         ###   ########.fr       */
+/*   Created: 2024/01/04 18:12:56 by tcharuel          #+#    #+#             */
+/*   Updated: 2024/01/04 18:13:40 by tcharuel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FDF_H
-# define FDF_H
-
-# include "../libft/libft.h"
-# include "mlx.h"
-
-# define WINDOW_WIDTH 1920
-# define WINDOW_HEIGHT 1080
-
-typedef struct s_data
+int	create_trgb(int t, int r, int g, int b)
 {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}			t_data;
-
-void		my_mlx_pixel_put(t_data *data, int x, int y, int color);
-
-int			create_trgb(int t, int r, int g, int b);
-
-#endif
+	return (t << 24 | r << 16 | g << 8 | b);
+}
